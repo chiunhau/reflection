e6fc0e7b47042e73fbb297697ca4e57b1ffffc51
Chiunhau <chiunhau9@gmail.com>
2015-04-15 11:10:42 +0800

    good morning

--- js/main.js
+++ js/main.js
@@ -7,20 +7,19 @@
 
 	var canvas = document.getElementById('sourceCanvas');
 	var video = document.getElementById('video');
-	var width = $(window).width();
-		var height = $(window).height();	
-	var xParts = 300;
-	var yParts = 200;
+	var width = 800;
+	var height = 600;	
+	var xParts = 400;
+	var yParts = 300;
 	var xSide = Math.floor(width / xParts);
 	var ySide = Math.floor(height / yParts);
 	var center = view.center;
 	var currentFrame;
-	var counter = 0;
 	var blocks = [];
+	var okGo = false;
+	var sizeCounter = 150;
 
-
-	function getStream() {
-
+function getStream() {
 	navigator.getMedia = navigator.getUserMedia || 
 		navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia ||
@@ -35,6 +34,7 @@
  				video.src = window.URL.createObjectURL(localMediaStream);
  				video.onloadedmetadata = function(e) {
  					setTimeout(function(){ 
+ 						okGo = true;
  						onFrame(); 
  					}, 1000);
 		      
@@ -53,20 +53,36 @@ function onFrame(event) {
 	c.scale(-1, 1);
   c.drawImage(video, -width, 0, width, height);
   c.restore();
-  currentFrame = c.getImageData(0, 0, width, height);	
+  currentFrame = c.getImageData(0, 0, width, height);
 
-	var blocksNum = blocks.length;
+ 	var blocksNum = blocks.length;
 	for(var i = 0; i < blocksNum; i ++) {
 		blocks[i].update();
 	}
 
-	if (okGO) {
-		var newBlock = new Block();
-		blocks.push(newBlock);
-	}
-	
+	if (blocksNum >= 10000) {
+		okGo = false;
+	};
 
-	
+	if (okGo) {
+		sizeCounter *= 0.999;
+		for (var i = 0; i < 1; i++) {
+
+			var newBlock = new Block();
+			blocks.push(newBlock);
+		};
+		
+		/*
+		counter += 1;
+		if (counter === 10) {
+			var newBlock = new Block();
+			blocks.push(newBlock);
+			counter = 0;
+		}
+		*/
+
+
+	}
 }
 
 function Block() {
@@ -77,8 +93,11 @@ function Block() {
 	var blockCenterY = randomR * ySide + Math.floor(ySide / 2);
 
 	this.pixelPos = (width * 4) * blockCenterY + blockCenterX * 4; //number
-	this.radius = 50;
-	this.path = new Path.Circle(new Point(blockCenterX, blockCenterY), 20);
+	this.radius = sizeCounter; 
+	var topLeft = new Point(blockCenterX, blockCenterY);
+	var rectSize = new Size(this.radius, this.radius);
+	var rectangle = new Rectangle(topLeft, rectSize);
+	this.path = new Path.Rectangle(rectangle);
 }
 
 Block.prototype.update = function() {
@@ -86,7 +105,6 @@ Block.prototype.update = function() {
 	var g = currentFrame.data[this.pixelPos + 1];
 	var b = currentFrame.data[this.pixelPos + 2];
 	this.path.fillColor = 'rgb('+ r + ',' + g + ',' + b + ')';
-	console.log("ha");
 }
 
 
